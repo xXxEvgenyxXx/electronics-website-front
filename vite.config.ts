ded48b13cb400fd3cdc5a8ff98d9478d56c1d143
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-19 16:34:04 +0300

    feat: connect frontend & backend

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,13 @@ export default defineConfig({
     alias: {
       '@':resolve(__dirname,'src'),
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin:true
+      }
+    }
   }
 })
