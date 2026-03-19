f4328e8e1ce05ddedb64c22c5281c1e5cbabbe07
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-19 10:20:05 +0300

    feat: add login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,46 @@
+import { Form, Input, Button } from "antd"
+//import type { FormProps } from "antd"
+import s from './LoginPage.module.scss'
+import { Link } from "react-router"
+
+//interface LoginFormType {
+//    login: string,
+//    email:string,
+//    password: string,
+//    name: string,
+//    surname:string,
+//    patronymic?:string
+//}
 
 export function LoginPage(){
     return (
-        <div>
-            
+        <div className={s.formWrapper}>
+            <Form
+                className={s.form}
+                name="login"
+                autoComplete="on"
+            >
+                <Form.Item
+                    label="Логин"
+                    name="login"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Пароль"
+                    name="password"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input type="password" />
+                </Form.Item>
+                <Button htmlType="submit">
+                    Войти
+                </Button>
+                <p>
+                    Не зарегистрированы? <Link to="/register">Зарегистрироваться</Link>
+                </p>
+            </Form>
         </div>
     )
 }
