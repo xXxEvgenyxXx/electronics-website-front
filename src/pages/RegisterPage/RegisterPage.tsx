c3ee26040ff8b9366a6003367ae802b86d9f1a5b
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-19 13:04:09 +0300

    feat: add reg page

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,74 @@
+import { Form, Input, Button } from "antd"
+//import type { FormProps } from "antd"
+import s from './RegisterPage.module.scss'
+import { Link } from "react-router"
+
+//interface LoginFormType {
+//    login: string,
+//    email:string,
+//    password: string,
+//    name: string,
+//    surname:string,
+//    patronymic?:string
+//}
+
 export function RegisterPage(){
     return (
-        <div>
-            
+        <div className={s.formWrapper}>
+            <Form
+                className={s.form}
+                name="register"
+                autoComplete="off"
+            >
+                <Button href="/" className={s.backButton} type="link" icon="&lt;" />
+                <Form.Item
+                    label="Имя"
+                    name="name"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Фамилия"
+                    name="surname"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Отчество (при наличии)"
+                    name="patronymic"
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Электронная почта"
+                    name="email"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input type="email" />
+                </Form.Item>
+                <Form.Item
+                    label="Логин"
+                    name="login"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Пароль"
+                    name="password"
+                    rules={[{ required: true, message: 'Это поле обязательно'}]}
+                >
+                    <Input type="password" />
+                </Form.Item>
+                <Button htmlType="submit">
+                    Войти
+                </Button>
+                <p>
+                    Уже зарегистрированы? <Link to="/login">Войти</Link>
+                </p>
+            </Form>
         </div>
     )
 }
