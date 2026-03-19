2ec35334c9257f2f40bc34a260a04fa87300692c
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-19 11:25:57 +0300

    feat: add footer

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,9 +1,32 @@
 import s from './Footer.module.scss'
+import { Logo } from '../Logo'
+import { Link } from 'react-router'
 
 export function Footer(){
     return (
         <footer className={s.footer}>
-            Подвал
+            <Logo />
+            <div className={s.linksWrapper}>
+                <h3>Поддержка</h3>
+                <p>Малая Грузинская ул., 27/13, Москва, 123557</p>
+                <p>electrograd@gmail.com</p>
+                <p>+7 965 111 11-11</p>
+            </div>
+            <div className={s.linksWrapper}>
+                <h3>Быстрые ссылки</h3>
+                <p>
+                    <Link className={s.link} to="/">Главная</Link>
+                </p>
+                <p>
+                    <Link className={s.link} to="/catalog">Каталог</Link>
+                </p>
+                <p>
+                    <Link className={s.link} to="/about">О нас</Link>
+                </p>
+                <p>
+                    <Link className={s.link} to="/contacts">Контакты</Link>
+                </p>
+            </div>
         </footer>
     )
 }
