167d97fdf98d72d2197ac06f626229f42e2e0c7b
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-21 13:48:04 +0300

    feat: add mockdata

---
+++ src/shared/mockData/brands.ts
@@ -0,0 +1,22 @@
+export const brands = [
+    {
+        name: "Bork",
+        image: "/images/bork.png"
+    },
+    {
+        name: "Samsung",
+        image: "/images/samsung.png"
+    },
+    {
+        name: "Яндекс",
+        image: "/images/yandex.png"
+    },
+    {
+        name: "Smeg",
+        image: "/images/smeg.png"
+    },
+    {
+        name: "LG",
+        image: "/images/lg.png"
+    },
+]
---
+++ src/shared/mockData/categories.ts
@@ -0,0 +1,34 @@
+export const categories = [
+    {
+        name: "Обогреватели",
+        image: "/images/heater.png"
+    },
+    {
+        name: "Телевизоры",
+        image: "/images/tv.png"
+    },
+    {
+        name: "Ноутбуки",
+        image: "/images/laptop.png"
+    },
+    {
+        name: "Чайники",
+        image: "/images/teapot.png"
+    },
+    {
+        name: "Холодильники",
+        image: "/images/fridge.png"
+    },
+    {
+        name: "Смартфоны",
+        image: "/images/smartphone.png"
+    },
+    {
+        name: "Наушники",
+        image: "/images/headphones.png"
+    },
+    {
+        name: "Гладильные системы",
+        image: "/images/iron.png"
+    },
+]
