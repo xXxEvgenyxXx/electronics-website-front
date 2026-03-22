a201c435828a4c756b19e94a3344f9a30cac4f54
Симонов Евгений Дмитриевич <jdsimonov2@gmail.com>
2026-03-22 19:35:20 +0300

    feat: add buttons in profile page

--- src/widgets/UserData/UserData.tsx
+++ src/widgets/UserData/UserData.tsx
@@ -1,12 +1,41 @@
 import s from './UserData.module.scss'
 import { UserOutlined } from "@ant-design/icons";
 import { UserDataView } from './UserDataView/UserDataView';
+import { Button } from 'antd';
+import { EditOutlined, SaveOutlined, CloseOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { UserDataEdit } from './UserDataEdit/UserDataEdit';
 
 export function UserData(){
+
+    const [isEditable, toggleProfileEditability] = useState(false);
+
+    function handleClick(){
+        toggleProfileEditability(!isEditable)
+    }
+    
+    function handleSave(){
+        handleClick()
+    }
+
     return (
         <div className={s.userDataWrapper}>
             <UserOutlined className={s.avatar}/>
-            <UserDataView/>
+            {!isEditable && (
+                <>
+                    <UserDataView/>
+                    <Button onClick={handleClick} icon={<EditOutlined/>}>Редактировать профиль</Button>
+                </>
+            )}
+            {isEditable && (
+                <>
+                    <UserDataEdit/>
+                    <div className={s.buttonsWrapper}>
+                        <Button onClick={handleSave} icon={<SaveOutlined/>}>Сохранить</Button>
+                        <Button onClick={handleClick} icon={<CloseOutlined/>}>Отмена</Button>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
